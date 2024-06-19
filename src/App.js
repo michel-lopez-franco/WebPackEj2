6388d1b38cafbf77cff206b373b8bb489ca2719b
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2024-06-19 16:38:07 -0600

    minificando el codigo para produccion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 //import React from "react";
+import { useState } from "react";
 
-const App = () =>( 
+/*const App = () =>( 
 <div className="container">
 <h1>Hello, World!</h1> 
 </div>
-);
+);*/
+
+const App = () => {
+ const [counter, setCounter] = useState(0);
+ const [values, setValues] = useState();
+
+ const handleClick = () => {
+     setCounter(counter + 1);
+     setValues(values.concat(counter));
+ }
+
+ return (
+    <div className="container">
+        <h1>Hello, World!</h1>
+        <button onClick={handleClick}>Click me!</button>
+        <div>
+           <strong> {counter} </strong> 
+        </div>
+    </div>
+
+    
+ )
+}
 
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 const ruleForStyles = {
     test: /\.css$/,
@@ -21,16 +23,28 @@ const ruleForJavaScript = {
     }
 
 
-const rules = [ ruleForJavaScript, ruleForStyles];
+const rules = [ ruleForJavaScript,ruleForStyles];
 
 module.exports = {
     //entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
     },
+    plugins:[
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ],
     module: {
         rules: rules ,
     },
+    devServer: {
+        open: true, // Open the page in browser
+        port: 3000,
+        liveReload: true, // Reload the page when the file changes
+        //contentBase: path.join(__dirname, 'build'), // Serve from the build folder
+        compress: true // Enable gzip compression
+        
+    },
+    //devtool: 'source-map'
 }
 
 
